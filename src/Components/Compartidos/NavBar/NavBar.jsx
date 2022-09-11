104f4e2ba61b1d982a3581c073165feabdbe1ba6
zstrings <95098967+zstrings@users.noreply.github.com>
2022-09-11 08:56:22 +0000

    log message

--- src/Components/Compartidos/NavBar/NavBar.jsx
+++ src/Components/Compartidos/NavBar/NavBar.jsx
@@ -20,4 +20,46 @@ const navBarStyles = makeStyles((theme) => ({
   sectionDesktop: {
     display: "none",
     [theme.breakpoints.up("md")]: {
-  
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  grow: { flexGrow: 1 },
+  growmid: { flexGrow: 0.05 },
+}));
+
+const logoStyles = makeStyles((theme) => ({
+  largeLogo: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
+const NavBar = (props) => {
+  const { navegacion } = props;
+  const classes = logoStyles();
+  const classesNavBar = navBarStyles();
+
+  const redirect = (path) => {
+    //window.location.replace("/jp");
+    console.log("Redicreccion desde NavBar");
+    props.history.push(`${path}`);
+  };
+  const onNavigateHome = () => {
+    window.location.replace("/");
+  };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClickAvatarMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseAvatarMenu = () => {
+    setAnchorEl(null);
+  };
+  const listaNavegacionLocal = navegacion 
