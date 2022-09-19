aa673d540c9491c5502fd2a2fb2aa480da774943
zstrings <95098967+zstrings@users.noreply.github.com>
2022-09-19 21:02:49 +0000

    Library updates

--- src/Components/Compartidos/__legacy/Footer.jsx
+++ src/Components/Compartidos/__legacy/Footer.jsx
@@ -49,3 +49,117 @@ const Footer2 = (props) => {
                       <li>
                         <a href="#0">
                           <i className="fab fa-dribbble"></i>{" "}
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6">
+              <div className="footer-widget footer-menu">
+                <h2 className="footer-title">For Patients</h2>
+                <ul>
+                  <li>
+                    <Link to="/patient/search-doctor">Search for Doctors</Link>
+                  </li>
+                  <li>
+                    <Link to="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link to="/register">Register</Link>
+                  </li>
+                  <li>
+                    <Link to="/patient/booking">Booking</Link>
+                  </li>
+                  <li>
+                    <Link to="/patient/dashboard">Patient Dashboard</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6">
+              <div className="footer-widget footer-menu">
+                <h2 className="footer-title">For Doctors</h2>
+                <ul>
+                  <li>
+                    <Link to="/doctor/appointments">Appointments</Link>
+                  </li>
+                  <li>
+                    <Link to="/doctor/chat-doctor">Chat</Link>
+                  </li>
+                  <li>
+                    <Link to="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link to="/doctor/doctor-register">Register</Link>
+                  </li>
+                  <li>
+                    <Link to="/doctor/doctor-dashboard">Doctor Dashboard</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-3 col-md-6">
+              <div className="footer-widget footer-contact">
+                <h2 className="footer-title">Contact Us</h2>
+                <div className="footer-contact-info">
+                  <div className="footer-address">
+                    <span>
+                      <i className="fa fa-map-marker" aria-hidden="true"></i>
+                    </span>
+                    <p>
+                      {" "}
+                      3556 Beech Street, San Francisco,California, CA 94108{" "}
+                    </p>
+                  </div>
+                  <p>
+                    <i className="fa fa-phone" aria-hidden="true"></i>
+                    +1 315 369 5943
+                  </p>
+                  <p className="mb-0">
+                    <i className="fa fa-envelope" aria-hidden="true"></i>
+                    doccure@example.com
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <div className="container-fluid">
+          <div className="copyright">
+            <div className="row">
+              <div className="col-md-6 col-lg-6">
+                <div className="copyright-text">
+                  <p className="mb-0">
+                    &copy; 2020 Doccure. All rights reserved.
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-6 col-lg-6">
+                <div className="copyright-menu">
+                  <ul className="policy-menu">
+                    <li>
+                      <Link to="/terms">Terms and Conditions</Link>
+                    </li>
+                    <li>
+                      <Link to="/privacy-policy">Policy</Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer2;
---
+++ src/Components/Genericos/JMaterialTable/JTableSSJ.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+  useTheme,
+} from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
