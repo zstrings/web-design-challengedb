bd7a640eaca2ca4e3e1654240cf50679855749c6
zstrings <95098967+zstrings@users.noreply.github.com>
2022-09-19 12:01:31 +0000

    Add ignore

--- src/Components/Compartidos/TopHeader/TopHeader.jsx
+++ src/Components/Compartidos/TopHeader/TopHeader.jsx
@@ -11,4 +11,38 @@ export const dummyLoation = [
 ];
 
 const TopHeader = (props) => {
-  const { subtitulo 
+  const { subtitulo } = props;
+  const handleClick = () => {};
+
+  /*   const localization = dummyLoation.slice(1);
+  const currentPage = dummyLoation[dummyLoation.length - 1];
+ */
+  /* console.log(
+    "localization",
+    props.history.location.pathname.split("/").splice(1) || ""
+  ); */
+  const makeRef = (listPath) => {
+    // console.log("makeref ini", listPath);
+    let listRef = [];
+    let j = 1;
+    for (let i = 0; i < listPath.length; i++) {
+      let ref = "";
+      for (let k = 0; k < j; k++) {
+        ref += `/${listPath[k]}`;
+      }
+      listRef.push({ page: listPath[i], href: ref });
+    }
+    //console.log("makeref fin", listRef);
+
+    return listRef;
+  };
+  let listPath;
+  let currentPage = "";
+  let localization = [];
+  if (!props.history) {
+    listPath = "";
+  } else {
+    listPath = props.history.location.pathname.split("/").splice(1);
+    const listRef = makeRef(listPath);
+    console.log("reff", listRef);
+    currentPage 
