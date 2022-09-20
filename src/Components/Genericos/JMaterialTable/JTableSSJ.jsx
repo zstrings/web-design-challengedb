46a9e63d4c90f793ad3eed10097a2b1660d5a245
zstrings <95098967+zstrings@users.noreply.github.com>
2022-09-20 14:54:53 +0000

    USB: option: add a D-Link DWM-156 variant

--- src/Components/Genericos/JMaterialTable/JTableSSJ.jsx
+++ src/Components/Genericos/JMaterialTable/JTableSSJ.jsx
@@ -10,3 +10,29 @@ import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
+import * as locales from "@material-ui/core/locale";
+import {
+  Avatar,
+  TableHead,
+  Toolbar,
+  Typography,
+  withStyles,
+  Zoom,
+} from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    // backgroundColor: theme.palette.primary.main,
+    backgroundColor: "white",
+
+    //color: theme.palette.common.white,
+    color: theme.palette.prim
