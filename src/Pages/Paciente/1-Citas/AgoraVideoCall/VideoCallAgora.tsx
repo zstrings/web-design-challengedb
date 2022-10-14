df57062843b8a7ad5714258cdeee6b9f46e11f99
zstrings <95098967+zstrings@users.noreply.github.com>
2022-10-14 18:56:41 +0000

    rt2x00: check if device is still available on rt2x00mac_flush()

--- src/Pages/Paciente/1-Citas/AgoraVideoCall/VideoCallAgora.tsx
+++ src/Pages/Paciente/1-Citas/AgoraVideoCall/VideoCallAgora.tsx
@@ -13,4 +13,23 @@ import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/Exp
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+
+// Declarative Stream Player for React 
+// Wrapped around native HTML video and audio tag with added Agora features
+import StreamPlayer from "agora-stream-player";
+
+import { SnackbarProvider, useSnackbar } from "notistack";
+
+// These customs hooks let any components in the application 
+// to directly use the required parameters and create clean functional components.
+// useCamera hook returns a list of cameras when the hook is called
+//
