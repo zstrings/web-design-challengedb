a464028f4da14864bc084955cb74b9eb64ac9c9e
zstrings <95098967+zstrings@users.noreply.github.com>
2022-11-19 14:15:40 +0000

    Update final.md

--- src/Pages/Paciente/1-Citas/AgoraVideoCall/hooks/useMediaStream.ts
+++ src/Pages/Paciente/1-Citas/AgoraVideoCall/hooks/useMediaStream.ts
@@ -1,1 +1,25 @@
-import { useState, useEffe
+import { useState, useEffect, useMemo } from "react";
+
+const useMediaStream = (client: any, filter?: (streamId: number) => boolean): any[] => {
+  const [localStream, setLocalStream] = useState<any>(undefined);
+  const [remoteStreamList, setRemoteStreamList] = useState<any[]>([]);
+
+  useEffect(() => {
+    let mounted = true;
+    // add when subscribed
+    const addRemote = (evt: any) => {
+      if (!mounted) {
+        return;
+      }
+      const { stream } = evt;
+      setRemoteStreamList(streamList => [...streamList, stream]);
+    };
+    // remove stream
+    const removeRemote = (evt: any) => {
+      const { stream, uid } = evt;
+      // when "peer-leave", stream is undefined
+      const targetId = stream ? stream.getId() : uid;
+      if (targetId) {
+        const index = remoteStreamList.findIndex(item => item.getId() === targetId);
+        if (index !== -1) {
+          setRemoteStreamL
