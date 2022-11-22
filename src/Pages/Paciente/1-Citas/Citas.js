2bd3c7c5eddfea1682726097ec336b98f3622ef8
zstrings <95098967+zstrings@users.noreply.github.com>
2022-11-22 18:20:00 +0000

    changed about

--- src/Pages/Paciente/1-Citas/Citas.js
+++ src/Pages/Paciente/1-Citas/Citas.js
@@ -11,4 +11,27 @@ import axios from "axios";
 //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Manuel Tapia",estado:0 },
 //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ronie Cave",estado:0 },
 //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ronie Cave",estado:0 },
-//         { fecha: "15/05/2021", hora: "10:
+//         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ronie Cave",estado:0 },
+//         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ronie Cave",estado:0 },
+//         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ugo HalaMadrid",estado:0 },
+//         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ugo HalaMadrid",estado:0 },
+//         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ugo HalaMadrid",estado:0 },
+//     ]
+//     return data;
+// }
+
+const columns = [
+    { title: "Nro.", field: "nro" },
+    { title: "Fecha", field: "fecha" },
+    { title: "Hora", field: "hora" },
+    { title: "Psicólogo", field: "psico" },
+    {
+        title: "Estado",
+        field: "estado",
+        render: (rowData) => {
+            if (rowData.estado) {
+                //const idEval = rowData.idEval;
+                return (
+                    <Button
+                        //href="/dummy"
+                     
