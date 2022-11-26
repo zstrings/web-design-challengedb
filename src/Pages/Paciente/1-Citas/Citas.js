548d2068eb2e0d8f13a168feedd404938e0ea523
zstrings <95098967+zstrings@users.noreply.github.com>
2022-11-26 07:29:12 +0000

    Update example.

--- src/Pages/Paciente/1-Citas/Citas.js
+++ src/Pages/Paciente/1-Citas/Citas.js
@@ -57,4 +57,19 @@ const Citas = (props) => {
     //     const data = [
     //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Manuel Tapia", estado: 1 },
     //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Manuel Tapia", estado: 1 },
-    //         { fecha: "15/05/2021", hora:
+    //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Manuel Tapia", estado: 0 },
+    //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ronie Cave", estado: 0 },
+    //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ronie Cave", estado: 0 },
+    //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ronie Cave", estado: 0 },
+    //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ronie Cave", estado: 0 },
+    //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ugo HalaMadrid", estado: 0 },
+    //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ugo HalaMadrid", estado: 0 },
+    //         { fecha: "15/05/2021", hora: "10:00 - 11:00", psico: "Ugo HalaMadrid", estado: 0 },
+    //     ]
+    //     return data;
+    // }
+
+    const [data, setData] = React.useState([]);
+    //const [loading, setLoading] = React.useState(true);
+
+    //async function setRows(
