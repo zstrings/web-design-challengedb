883656c74dffccba1163705e60bc418d79fbef0c
zstrings <95098967+zstrings@users.noreply.github.com>
2022-12-08 15:58:33 +0000

    Small modifications

--- src/Pages/Psicologo/PsicologoMainPage.js
+++ src/Pages/Psicologo/PsicologoMainPage.js
@@ -3,4 +3,23 @@ import PageWrapper from '../../Components/Compartidos/PageWrapper';
 import SesionVideoLlamada from '../Paciente/1-Citas/SesionVideoLlamada';
 import Agenda from './1-Agenda/Agenda';
 import CitasXPaciente from './2-Pacientes/CitasXPaciente';
-import DiarioTextoXPaciente from './2-Pacientes/DiarioTextoXPacie
+import DiarioTextoXPaciente from './2-Pacientes/DiarioTextoXPaciente';
+import DirarioAudioXPaciente from './2-Pacientes/DirarioAudioXPaciente';
+import PerfilXPaciente from './2-Pacientes/PerfilXPaciente';
+import PlanDeAccionXPaciente from './2-Pacientes/PlanDeAccionXPaciente';
+import GruposDeApoyo from './3-GruposDeApoyo/GruposDeApoyo';
+import PacientesXGrupoDeApoyo from './3-GruposDeApoyo/PacientesXGrupoDeApoyo';
+import {  Route } from "react-router-dom";
+
+const PsicologoMainPage =props=> {
+    const [subtitulo, setSubtitulo] = React.useState(undefined);
+    const makeSubtitulo = (_subtitulo) => {
+        setSubtitulo(_subtitulo);
+    }
+    useEffect(()=>{
+        document.title="Serenity | Psicólogo"
+    });
+        return (
+            <PageWrapper {...props} subtitulo={subtitulo}>
+                <Route exact path="/psicologo" component={(props)=><Agenda {...props} makeSubtitulo={makeSubtitulo}/>} />
+                <Route exact path="/psicologo/age
