a7199e7134e79280f805885a2a177f042062f001
zstrings <95098967+zstrings@users.noreply.github.com>
2023-01-07 09:47:09 +0000

    Update swlinks.txt

---
+++ src/redux/backdrop.js
@@ -0,0 +1,12 @@
+
+import { createContext, useContext, useReducer } from "react";
+
+export const BackDropContext = createContext();
+
+export const BackDropProvider = ({ reducer, initialState, children }) => (
+  <BackDropContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </BackDropContext.Provider>
+);
+
+export const useBackDropValue = () => useContext(BackDropContext);
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,5 @@
+import sesionReducer from "./sesionReducer";
+import openDialogReducer from "./openDialogReducer";
+
+export const mainReducer = ({sesion, openDialog},action) => {
+    
