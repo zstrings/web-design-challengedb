22f511a5e676c99829ff34ef5cfeb2628370f8a7
zstrings <95098967+zstrings@users.noreply.github.com>
2023-01-07 14:20:45 +0000

    Init code

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,4 +2,8 @@ import sesionReducer from "./sesionReducer";
 import openDialogReducer from "./openDialogReducer";
 
 export const mainReducer = ({sesion, openDialog},action) => {
-    
+    return {
+        sesion: sesionReducer(sesion,action),
+        openDialog: openDialogReducer(openDialog,action) 
+    }
+}
